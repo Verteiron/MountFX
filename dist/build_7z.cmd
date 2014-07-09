ee51072d0a2139bb1ad990ce23c882d41a910467
Verteiron <verteiron@gmail.com>
2014-07-08 23:58:20 -0500

    build scripts

---
+++ dist/build_7z.cmd
@@ -0,0 +1,21 @@
+@echo off
+SET FILENAME=vMFX_MountFX
+SET ZEXE=c:\Program Files\7-Zip\7z.exe
+
+for /f "tokens=1,2,3,4 delims=^/ " %%W in ('@echo %date%') DO SET NEWTIME=%%Z-%%X-%%Y
+for /f "tokens=1,2,3 delims=:." %%W in ('@echo %time%') DO SET NEWTIME=%NEWTIME%_%%W-%%X-%%Y
+
+echo %NEWTIME%
+mkdir "%NEWTIME%_%COMPUTERNAME%"
+cd "%NEWTIME%_%COMPUTERNAME%"
+xcopy /y ..\data\*.esp .
+xcopy /y ..\data\*.bsa .
+xcopy /y ..\data\*readme* .
+xcopy /y ..\..\doc\*readme* .
+echo d | xcopy /e /y ..\data\skse skse
+echo d | xcopy /e /y ..\data\vMFX vMFX
+"%ZEXE%" a -r "vMFX_BakedBeans_%NEWTIME%.7z" "*"
+xcopy /y *.7z ..
+cd ..
+ping -n 1 -w 1000 1.0.0.0 > nul
+rmdir /s /q "%NEWTIME%_%COMPUTERNAME%"
